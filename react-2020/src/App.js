c0406ccbc737dec5bbbc3a6e8a42bf8f32477e53
rolu225 <trangiaky2@gmail.com>
2020-12-28 16:31:12 +0700

    update components

--- react-2020/src/App.js
+++ react-2020/src/App.js
@@ -1,11 +1,88 @@
-import React from 'react'
-import Setup from './tutorial/12-memo-useMemo-useCallback/setup'
+import React, {useState, useEffect, useCallback} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom'
+//PAGE
 
-function App() {
-  return(
+const url = 'https://course-api.com/javascript-store-products'
+
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const getProducts = useCallback(async () => {
+      const response = await fetch(url);
+      const products = await response.json();
+      setProducts(products);
+      setLoading(false);
+  }, [url]);
+
+  useEffect(() => {
+      getProducts();
+  },[url, getProducts])
+  return {loading, products};
+}
+
+const App = () => {
+  const {products} = useFetch(url)
+  const [cart, setCart] = useState(0)
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1)
+  }, [cart])
+  
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to='/dashboard' />
+        </Route>
+        <Route path='/dashboard' render={() => <BigList cart={cart} products={products} addToCart={addToCart} />} />
+      </Switch>
+    </Router>
+  )
+}
+
+//nav  
+const BigList = React.memo(({ products, addToCart, cart }) => {
+  return (
     <div>
-      <Setup/>
+      <div id='fixNav'>
+        <button type="button" className="btn btn-success" aria-label="Left Align" >
+        <span className="glyphicon glyphicon-shopping-cart" aria-hidden="true">{cart}</span>
+        </button> 
+      </div>
+      <br/>
+      <div className='products'>
+        {products.map((product) =>{
+          return(
+            <SingleProduct
+              key={product.id}
+              {...product}
+              addToCart={addToCart}
+            ></SingleProduct>
+          )
+        })}
+      </div>
     </div>
+  );
+})
+
+export const SingleProduct = ({fields, addToCart}) => {
+  let {name, price} = fields
+  price = price / 100;
+  const image = fields.image[0].url
+  return (
+    <section className='product btn'>
+      <Link to={`/product/${name}`}>
+          <div>
+            <img src={image} alt={name} />
+            <h3>{name}</h3>
+            <h4>${price}</h4>
+          </div>
+      </Link>
+      <div>
+        <button onClick={addToCart} className="btnn">Add to card</button>
+      </div>
+    </section>
   )
 }
 
