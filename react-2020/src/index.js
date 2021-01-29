c9528e26005e7412a449496a2fd2de1021980d32
rolu225 <tgky2412@gmail.com>
2021-01-29 10:18:55 +0700

    react app polyfill

--- react-2020/src/index.js
+++ react-2020/src/index.js
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
